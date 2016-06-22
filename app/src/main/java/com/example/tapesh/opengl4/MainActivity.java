8f5e96d7d2f5cd4624c5d5b8def81e6a83a6f6e4
vipinvats42 <vipinvats42@gmail.com>
2016-06-22 18:06:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tapesh/opengl4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tapesh.opengl4;
+
+
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private CanvasView customCanvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+
+    }
+
+    public void clearCanvas(View v) {
+          customCanvas.clearCanvas();
+         }
+
+}
